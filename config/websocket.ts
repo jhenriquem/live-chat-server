974d2dda852496bcce89316a8589dea9ed0719e9
jhenriquem <machadojh11@gmail.com>
2024-11-23 16:38:59 -0300

    websocket framework finished
    Configurações finais e algumas correções

--- config/websocket.ts
+++ config/websocket.ts
@@ -3,19 +3,33 @@ import { WebSocket, WebSocketServer } from "ws";
 import messagesInterface from "../types/messagesType"
 import messageModel from "../models/messagesModel";
 
+interface MsgInterface {
+  action: string,
+  data: messagesInterface
+}
+
+interface ResponseI {
+  action: string,
+  list?: messagesInterface[]
+  messageAdded?: messagesInterface
+}
+
+const ACTIONS = {
+  NM: "RETURN NEW MESSAGE",
+  ML: "RETURN MESSAGE LIST",
+}
+
 export default class WebSocketHandler {
 
   private wsServer: WebSocketServer;
 
   constructor(server: Server) {
     this.wsServer = new WebSocketServer({ server })
-
     this.setupConnection()
   }
 
   private setupConnection() {
     this.wsServer.on("connection", (crrClient) => {
-
 
       // Mandar as mensagens para o client que se conectou 
       this.handleSendMessagesToClient(crrClient)
@@ -26,16 +40,40 @@ export default class WebSocketHandler {
     })
   }
 
-
   private handleClientMessage(wsClient: WebSocket) {
-    // Quando o client postar uma mensagem no chat, ao postar, ele também manda
-    // essa mensagem para o servidor para ser adicionada do banco de dados 
-    // Tal metodo, trata desse evento
 
-    wsClient.on("message", (data) => {
-      //...
+    wsClient.on("message", async (msg: string) => {
+
+      console.log("Mensagem Recebida")
+      const data: MsgInterface = JSON.parse(msg)
+
+      const newMessage = new messageModel(data.data)
+      const res: ResponseI = {
+        action: ACTIONS.NM,
+        messageAdded: data.data
+      }
+      try {
+        newMessage.save()
+      }
+      catch (err: any) {
+        console.log(err.message)
+        console.log(data)
+        //res.status = "Error"
+        //res.message = err.message
+      }
+      wsClient.send(JSON.stringify(res))
+
+      const messagesList: messagesInterface[] = await messageModel.find({}, { _id: 0, __v: 0 })
+      const list = messagesList.includes(data.data) ? messagesList : [...messagesList, data.data]
+      Array.from(this.wsServer.clients).forEach((client) => {
+        if (wsClient !== client) {
+          client.send(
+            JSON.stringify(
+              { action: ACTIONS.ML, list: list })
+          )
+        }
+      })
     })
-
   }
 
   private async handleSendMessagesToClient(wsClient: WebSocket) {
@@ -44,7 +82,8 @@ export default class WebSocketHandler {
       try {
         const messagesList: messagesInterface[] = await messageModel.find()
 
-        wsClient.send(JSON.stringify({ list: messagesList }))
+        wsClient.send(JSON.stringify(
+          { action: ACTIONS.ML, list: messagesList }))
       }
       catch (err: any) {
         wsClient.send(JSON.stringify({
